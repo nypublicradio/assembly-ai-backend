e4068ae9e1ce9d3ba903ed454cf5bb7337a5aaa6
Kim LaRocca <kim@kimlarocca.com>
2023-06-12 21:01:03 -0400

    adding supabase to the project

---
+++ index.js
@@ -0,0 +1,101 @@
+require( 'dotenv' ).config();
+
+// create supabase client
+const {
+    createClient
+} = require( '@supabase/supabase-js' );
+const supabase = createClient( process.env.SUPABASE_URL, process.env.SUPABASE_KEY )
+
+// inserts a new record to the transcripts table
+const insertTranscript = async ( transcriptData ) => {
+    const {
+        error
+    } = await supabase
+        .from( 'transcripts' )
+        .insert( [ {
+            show_id: 1,
+            transcript: {
+                "text": transcriptData.text,
+                "created": transcriptData.created,
+                "audio_end": transcriptData.audio_end,
+                "confidence": transcriptData.confidence,
+                "audio_start": transcriptData.audio_start
+            }
+        }, ] )
+    if ( error ) {
+        console.log( 'insertTranscript error', error );
+    }
+}
+
+// updates the current partial transcript in the closedCaptioning table
+const updatePartialTranscript = async ( transcriptData ) => {
+    const {
+        error
+    } = await supabase
+        .from( 'closedCaptioning' )
+        .update( {
+            current_partial_transcript: transcriptData.text
+        } )
+        .eq( 'show_id', 1 )
+    if ( error ) {
+        console.log( 'updatePartialTranscript error', error );
+    }
+}
+
+const WebSocket = require( 'ws' );
+const spawn = require( 'child_process' ).spawn;
+
+const ws = new WebSocket( 'wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000', {
+    headers: {
+        Authorization: process.env.ASSEMBLYAI_API_KEY,
+    }
+} );
+
+ws.on( 'open', function open() {
+    const ffmpeg = spawn( 'ffmpeg', [ '-re', '-i', process.env.HLS_URL, '-vn', '-f', 's16le', '-ac', '1', '-ar', '16000', '-' ] );
+
+    let buffer = Buffer.alloc( 0 );
+
+    ffmpeg.stdout.on( 'data', ( data ) => {
+        buffer = Buffer.concat( [ buffer, data ] );
+        const buffer_size = 4096;
+        while ( buffer.length >= buffer_size ) {
+            const chunk = buffer.slice( 0, buffer_size );
+            buffer = buffer.slice( buffer_size );
+
+            if ( ws.readyState === WebSocket.OPEN ) {
+                ws.send( JSON.stringify( {
+                    audio_data: chunk.toString( 'base64' )
+                } ), ( error ) => {
+                    if ( error ) console.error( error );
+                } );
+            }
+        }
+    } );
+
+    ffmpeg.stderr.on( 'data', ( data ) => {
+        // console.error(`stderr: ${data}`);
+    } );
+
+    ffmpeg.on( 'close', ( code ) => {
+        console.log( `child process exited with code ${code}` );
+    } );
+
+    ws.on( 'close', () => {
+        ffmpeg.kill();
+    } );
+} );
+
+ws.on( 'message', function incoming( message ) {
+    const data = JSON.parse( message );
+    // save the current partial transcript to the database
+    if ( data.message_type === 'PartialTranscript' ) {
+        console.log( 'PartialTranscript', data.text );
+        updatePartialTranscript( data );
+    }
+    // save the final transcript to the database
+    if ( data.message_type === 'FinalTranscript' ) {
+        console.log( 'FinalTranscript', data.text );
+        insertTranscript( data );
+    }
+} );
---
+++ sample-data.js
@@ -0,0 +1,777 @@
+const data = [ {
+  audio_start: 0,
+  audio_end: 19830,
+  confidence: 0.876503653694773,
+  text: "letter show the princeton bioethicist peter singer his nineteen seventy five book animal liberation helped launch the modern animal rights movement we'll talk about his new addition just released called animal liberation now also for the first day of pride month a call in for trans and nonbinary listeners on your experiences of",
+  words: [ {
+      confidence: 0.378251299949573,
+      text: 'letter',
+      start: 990,
+      end: 1145
+    },
+    {
+      confidence: 0.98427468797342,
+      text: 'show',
+      start: 1430,
+      end: 1465
+    },
+    {
+      confidence: 0.647178097848138,
+      text: 'the',
+      start: 1550,
+      end: 1585
+    },
+    {
+      confidence: 0.984560037734434,
+      text: 'princeton',
+      start: 1830,
+      end: 1985
+    },
+    {
+      confidence: 0.663744712137967,
+      text: 'bioethicist',
+      start: 2310,
+      end: 2905
+    },
+    {
+      confidence: 0.995639613276964,
+      text: 'peter',
+      start: 3230,
+      end: 3345
+    },
+    {
+      confidence: 0.504939665841531,
+      text: 'singer',
+      start: 3710,
+      end: 3865
+    },
+    {
+      confidence: 0.663016857627946,
+      text: 'his',
+      start: 4470,
+      end: 4505
+    },
+    {
+      confidence: 0.828399440291955,
+      text: 'nineteen',
+      start: 4590,
+      end: 4665
+    },
+    {
+      confidence: 0.999561978595442,
+      text: 'seventy',
+      start: 5030,
+      end: 5065
+    },
+    {
+      confidence: 0.988638551959661,
+      text: 'five',
+      start: 5150,
+      end: 5185
+    },
+    {
+      confidence: 0.999934677574784,
+      text: 'book',
+      start: 5510,
+      end: 5545
+    },
+    {
+      confidence: 0.637410756754947,
+      text: 'animal',
+      start: 5870,
+      end: 6065
+    },
+    {
+      confidence: 0.999368136691549,
+      text: 'liberation',
+      start: 6390,
+      end: 6785
+    },
+    {
+      confidence: 0.833367275646192,
+      text: 'helped',
+      start: 7110,
+      end: 7225
+    },
+    {
+      confidence: 0.999842546633532,
+      text: 'launch',
+      start: 7270,
+      end: 7345
+    },
+    {
+      confidence: 0.999285493594263,
+      text: 'the',
+      start: 7630,
+      end: 7665
+    },
+    {
+      confidence: 0.949031691682951,
+      text: 'modern',
+      start: 7750,
+      end: 7865
+    },
+    {
+      confidence: 0.999907500074306,
+      text: 'animal',
+      start: 8150,
+      end: 8265
+    },
+    {
+      confidence: 0.935689608852048,
+      text: 'rights',
+      start: 8550,
+      end: 8705
+    },
+    {
+      confidence: 0.999646307847479,
+      text: 'movement',
+      start: 8990,
+      end: 9105
+    },
+    {
+      confidence: 0.865148898169335,
+      text: "we'll",
+      start: 9470,
+      end: 9545
+    },
+    {
+      confidence: 0.998047238594522,
+      text: 'talk',
+      start: 9550,
+      end: 9585
+    },
+    {
+      confidence: 0.999856850134718,
+      text: 'about',
+      start: 9870,
+      end: 9905
+    },
+    {
+      confidence: 0.714259633123089,
+      text: 'his',
+      start: 9990,
+      end: 10025
+    },
+    {
+      confidence: 0.999992132248763,
+      text: 'new',
+      start: 10310,
+      end: 10345
+    },
+    {
+      confidence: 0.52129775575246,
+      text: 'addition',
+      start: 10430,
+      end: 10545
+    },
+    {
+      confidence: 0.994952063613012,
+      text: 'just',
+      start: 10870,
+      end: 10905
+    },
+    {
+      confidence: 0.727599001936708,
+      text: 'released',
+      start: 11270,
+      end: 11345
+    },
+    {
+      confidence: 0.999772719546593,
+      text: 'called',
+      start: 11710,
+      end: 11745
+    },
+    {
+      confidence: 0.565313597067682,
+      text: 'animal',
+      start: 12110,
+      end: 12265
+    },
+    {
+      confidence: 0.999202339865117,
+      text: 'liberation',
+      start: 12590,
+      end: 13065
+    },
+    {
+      confidence: 0.931688770082352,
+      text: 'now',
+      start: 13150,
+      end: 13185
+    },
+    {
+      confidence: 0.991912853771597,
+      text: 'also',
+      start: 13830,
+      end: 13865
+    },
+    {
+      confidence: 0.999921566441278,
+      text: 'for',
+      start: 14310,
+      end: 14345
+    },
+    {
+      confidence: 0.999937299843942,
+      text: 'the',
+      start: 14390,
+      end: 14425
+    },
+    {
+      confidence: 0.999200268927582,
+      text: 'first',
+      start: 14430,
+      end: 14465
+    },
+    {
+      confidence: 0.999888432550035,
+      text: 'day',
+      start: 14790,
+      end: 14825
+    },
+    {
+      confidence: 0.889650879503362,
+      text: 'of',
+      start: 14870,
+      end: 14905
+    },
+    {
+      confidence: 0.936574910783689,
+      text: 'pride',
+      start: 15150,
+      end: 15265
+    },
+    {
+      confidence: 0.997815147773234,
+      text: 'month',
+      start: 15310,
+      end: 15345
+    },
+    {
+      confidence: 0.620616524818242,
+      text: 'a',
+      start: 15910,
+      end: 15945
+    },
+    {
+      confidence: 0.99835060343164,
+      text: 'call',
+      start: 15990,
+      end: 16025
+    },
+    {
+      confidence: 0.977243685742252,
+      text: 'in',
+      start: 16110,
+      end: 16145
+    },
+    {
+      confidence: 0.954848653912408,
+      text: 'for',
+      start: 16190,
+      end: 16225
+    },
+    {
+      confidence: 0.580020379210136,
+      text: 'trans',
+      start: 16550,
+      end: 16585
+    },
+    {
+      confidence: 0.995270080824831,
+      text: 'and',
+      start: 16910,
+      end: 16945
+    },
+    {
+      confidence: 0.68022440908923,
+      text: 'nonbinary',
+      start: 16990,
+      end: 17505
+    },
+    {
+      confidence: 0.874279818359217,
+      text: 'listeners',
+      start: 17830,
+      end: 17985
+    },
+    {
+      confidence: 0.996078314742993,
+      text: 'on',
+      start: 18270,
+      end: 18305
+    },
+    {
+      confidence: 0.999910958590524,
+      text: 'your',
+      start: 18670,
+      end: 18705
+    },
+    {
+      confidence: 0.941317602753407,
+      text: 'experiences',
+      start: 19110,
+      end: 19265
+    },
+    {
+      confidence: 0.712811316029914,
+      text: 'of',
+      start: 19310,
+      end: 19345
+    }
+  ],
+  created: '2023-05-31T19: 21: 42.341824',
+  message_type: 'PartialTranscript'
+}, {
+  audio_start: 0,
+  audio_end: 20270,
+  confidence: 0.87963222529418,
+  text: "Letter Show. The princeton bioethicist Peter Singer, his 1975 book Animal Liberation, helped launch the modern animal rights movement. We'll talk about his new addition, just released, called Animal Liberation. Now, also, for the first day of Pride Month, a call in for trans and nonbinary listeners on your experiences of gender, eup.",
+  words: [ {
+      confidence: 0.378251299949573,
+      text: 'Letter',
+      start: 990,
+      end: 1145
+    },
+    {
+      confidence: 0.98427468797342,
+      text: 'Show.',
+      start: 1430,
+      end: 1465
+    },
+    {
+      confidence: 0.65,
+      text: 'The',
+      start: 1550,
+      end: 1585
+    },
+    {
+      confidence: 0.984560037734434,
+      text: 'princeton',
+      start: 1830,
+      end: 1985
+    },
+    {
+      confidence: 0.663744712137967,
+      text: 'bioethicist',
+      start: 2310,
+      end: 2905
+    },
+    {
+      confidence: 0.995639613276964,
+      text: 'Peter',
+      start: 3230,
+      end: 3345
+    },
+    {
+      confidence: 0.504939665841531,
+      text: 'Singer,',
+      start: 3710,
+      end: 3865
+    },
+    {
+      confidence: 0.663016857627946,
+      text: 'his',
+      start: 4470,
+      end: 4505
+    },
+    {
+      confidence: 0.94,
+      text: '1975',
+      start: 4590,
+      end: 5185
+    },
+    {
+      confidence: 0.999934677574784,
+      text: 'book',
+      start: 5510,
+      end: 5545
+    },
+    {
+      confidence: 0.637410756754947,
+      text: 'Animal',
+      start: 5870,
+      end: 6065
+    },
+    {
+      confidence: 0.999368136691549,
+      text: 'Liberation,',
+      start: 6390,
+      end: 6785
+    },
+    {
+      confidence: 0.833367275646192,
+      text: 'helped',
+      start: 7110,
+      end: 7225
+    },
+    {
+      confidence: 0.999842546633532,
+      text: 'launch',
+      start: 7270,
+      end: 7345
+    },
+    {
+      confidence: 1,
+      text: 'the',
+      start: 7630,
+      end: 7665
+    },
+    {
+      confidence: 0.949031691682951,
+      text: 'modern',
+      start: 7750,
+      end: 7865
+    },
+    {
+      confidence: 0.999907500074306,
+      text: 'animal',
+      start: 8150,
+      end: 8265
+    },
+    {
+      confidence: 0.935689608852048,
+      text: 'rights',
+      start: 8550,
+      end: 8705
+    },
+    {
+      confidence: 0.999646307847479,
+      text: 'movement.',
+      start: 8990,
+      end: 9105
+    },
+    {
+      confidence: 0.865148898169335,
+      text: "We'll",
+      start: 9470,
+      end: 9545
+    },
+    {
+      confidence: 0.998047238594522,
+      text: 'talk',
+      start: 9550,
+      end: 9585
+    },
+    {
+      confidence: 0.999856850134718,
+      text: 'about',
+      start: 9870,
+      end: 9905
+    },
+    {
+      confidence: 0.714259633123089,
+      text: 'his',
+      start: 9990,
+      end: 10025
+    },
+    {
+      confidence: 1,
+      text: 'new',
+      start: 10310,
+      end: 10345
+    },
+    {
+      confidence: 0.52129775575246,
+      text: 'addition,',
+      start: 10430,
+      end: 10545
+    },
+    {
+      confidence: 0.994952063613012,
+      text: 'just',
+      start: 10870,
+      end: 10905
+    },
+    {
+      confidence: 0.727599001936708,
+      text: 'released,',
+      start: 11270,
+      end: 11345
+    },
+    {
+      confidence: 0.999772719546593,
+      text: 'called',
+      start: 11710,
+      end: 11745
+    },
+    {
+      confidence: 0.565313597067682,
+      text: 'Animal',
+      start: 12110,
+      end: 12265
+    },
+    {
+      confidence: 0.999202339865117,
+      text: 'Liberation.',
+      start: 12590,
+      end: 13065
+    },
+    {
+      confidence: 0.931688770082352,
+      text: 'Now,',
+      start: 13150,
+      end: 13185
+    },
+    {
+      confidence: 0.991912853771597,
+      text: 'also,',
+      start: 13830,
+      end: 13865
+    },
+    {
+      confidence: 0.999921566441278,
+      text: 'for',
+      start: 14310,
+      end: 14345
+    },
+    {
+      confidence: 1,
+      text: 'the',
+      start: 14390,
+      end: 14425
+    },
+    {
+      confidence: 1,
+      text: 'first',
+      start: 14430,
+      end: 14465
+    },
+    {
+      confidence: 0.999888432550035,
+      text: 'day',
+      start: 14790,
+      end: 14825
+    },
+    {
+      confidence: 0.89,
+      text: 'of',
+      start: 14870,
+      end: 14905
+    },
+    {
+      confidence: 0.936574910783689,
+      text: 'Pride',
+      start: 15150,
+      end: 15265
+    },
+    {
+      confidence: 0.997815147773234,
+      text: 'Month,',
+      start: 15310,
+      end: 15345
+    },
+    {
+      confidence: 0.62,
+      text: 'a',
+      start: 15910,
+      end: 15945
+    },
+    {
+      confidence: 0.99835060343164,
+      text: 'call',
+      start: 15990,
+      end: 16025
+    },
+    {
+      confidence: 0.977243685742252,
+      text: 'in',
+      start: 16110,
+      end: 16145
+    },
+    {
+      confidence: 0.954848653912408,
+      text: 'for',
+      start: 16190,
+      end: 16225
+    },
+    {
+      confidence: 0.580020379210136,
+      text: 'trans',
+      start: 16550,
+      end: 16585
+    },
+    {
+      confidence: 1,
+      text: 'and',
+      start: 16910,
+      end: 16945
+    },
+    {
+      confidence: 0.68022440908923,
+      text: 'nonbinary',
+      start: 16990,
+      end: 17505
+    },
+    {
+      confidence: 0.874279818359217,
+      text: 'listeners',
+      start: 17830,
+      end: 17985
+    },
+    {
+      confidence: 0.996078314742993,
+      text: 'on',
+      start: 18270,
+      end: 18305
+    },
+    {
+      confidence: 0.999910958590524,
+      text: 'your',
+      start: 18670,
+      end: 18705
+    },
+    {
+      confidence: 0.941317602753407,
+      text: 'experiences',
+      start: 19110,
+      end: 19265
+    },
+    {
+      confidence: 0.71,
+      text: 'of',
+      start: 19310,
+      end: 19345
+    },
+    {
+      confidence: 0.928174796287395,
+      text: 'gender,',
+      start: 19910,
+      end: 20065
+    },
+    {
+      confidence: 0.996903949069528,
+      text: 'eup.',
+      start: 20150,
+      end: 20225
+    }
+  ],
+  created: '2023-05-31T19: 21: 42.788931',
+  message_type: 'FinalTranscript',
+  punctuated: true,
+  text_formatted: true
+}, {
+  audio_start: 20150,
+  audio_end: 20740,
+  confidence: 0.619647516018273,
+  text: 'euphoria',
+  words: [ {
+    confidence: 0.619647516018273,
+    text: 'euphoria',
+    start: 20150,
+    end: 20735
+  } ],
+  created: '2023-05-31T19: 21: 43.305705',
+  message_type: 'PartialTranscript'
+}, {
+  audio_start: 20270,
+  audio_end: 21180,
+  confidence: 0.619647516018273,
+  text: 'euphoria',
+  words: [ {
+    confidence: 0.619647516018273,
+    text: 'euphoria',
+    start: 20150,
+    end: 20735
+  } ],
+  created: '2023-05-31T19: 21: 43.683710',
+  message_type: 'PartialTranscript'
+}, {
+  audio_start: 20270,
+  audio_end: 21620,
+  confidence: 0.786241491481949,
+  text: 'euphoria the brian',
+  words: [ {
+      confidence: 0.619647516018273,
+      text: 'euphoria',
+      start: 20150,
+      end: 20735
+    },
+    {
+      confidence: 0.754201593524296,
+      text: 'the',
+      start: 21260,
+      end: 21295
+    },
+    {
+      confidence: 0.984875364903279,
+      text: 'brian',
+      start: 21380,
+      end: 21455
+    }
+  ],
+  created: '2023-05-31T19: 21: 44.073184',
+  message_type: 'PartialTranscript'
+}, {
+  audio_start: 20270,
+  audio_end: 22060,
+  confidence: 0.648092677183052,
+  text: 'euphoria the brian larsh',
+  words: [ {
+      confidence: 0.619647516018273,
+      text: 'euphoria',
+      start: 20150,
+      end: 20735
+    },
+    {
+      confidence: 0.754201593524296,
+      text: 'the',
+      start: 21260,
+      end: 21295
+    },
+    {
+      confidence: 0.984875364903279,
+      text: 'brian',
+      start: 21380,
+      end: 21455
+    },
+    {
+      confidence: 0.23364623428636,
+      text: 'larsh',
+      start: 21780,
+      end: 22015
+    }
+  ],
+  created: '2023-05-31T19: 21: 44.593918',
+  message_type: 'PartialTranscript'
+}, {
+  audio_start: 20270,
+  audio_end: 22500,
+  confidence: 0.674756895828392,
+  text: 'euphoria the brian larshow week',
+  words: [ {
+      confidence: 0.619647516018273,
+      text: 'euphoria',
+      start: 20150,
+      end: 20735
+    },
+    {
+      confidence: 0.754201593524296,
+      text: 'the',
+      start: 21260,
+      end: 21295
+    },
+    {
+      confidence: 0.984875364903279,
+      text: 'brian',
+      start: 21380,
+      end: 21455
+    },
+    {
+      confidence: 0.190383957521918,
+      text: 'larshow',
+      start: 21780,
+      end: 22255
+    },
+    {
+      confidence: 0.824676047174194,
+      text: 'week',
+      start: 22340,
+      end: 22375
+    }
+  ],
+  created: '2023-05-31T19: 21: 44.990045',
+  message_type: 'PartialTranscript'
+} ]
